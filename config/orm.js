b893252db84933273beed00c0fc459fad7f1b5c9
bthipavo <bthipavo@gmail.com>
2018-03-27 00:19:51 -0700

    added routers and models

--- config/orm.js
+++ config/orm.js
@@ -34,7 +34,7 @@ function objToSql(ob) {
 }
 // Object for all our SQL statement functions.
 var orm = {
-  all: function(tableInput, cb) {
+  selectAll: function(tableInput, cb) {
     var queryString = "SELECT * FROM " + tableInput + ";";
     connection.query(queryString, function(err, result) {
       if (err) {
@@ -43,7 +43,7 @@ var orm = {
       cb(result);
     });
   },
-  create: function(table, cols, vals, cb) {
+  insertOne: function(table, cols, vals, cb) {
     var queryString = "INSERT INTO " + table;
     queryString += " (";
     queryString += cols.toString();
@@ -60,24 +60,13 @@ var orm = {
     });
   },
   // An example of objColVals would be {name: panther, sleepy: true}
-  update: function(table, objColVals, condition, cb) {
+  updateOne: function(table, objColVals, condition, cb) {
     var queryString = "UPDATE " + table;
     queryString += " SET ";
     queryString += objToSql(objColVals);
     queryString += " WHERE ";
     queryString += condition;
     console.log(queryString);
-    connection.query(queryString, function(err, result) {
-      if (err) {
-        throw err;
-      }
-      cb(result);
-    });
-  },
-  delete: function(table, condition, cb) {
-    var queryString = "DELETE FROM " + table;
-    queryString += " WHERE ";
-    queryString += condition;
     connection.query(queryString, function(err, result) {
       if (err) {
         throw err;
---
+++ models/burger.js
@@ -0,0 +1,22 @@
+var orm = require('../config/orm.js')
+
+var burger = {
+	selectAll: function(cb) {
+		orm.selectAll("burgers", function(res) {
+			cb(res)
+		})
+	},
+
+	insertOne: function(id, burger_name, devoured, cb) {
+		orm.selectOne("burgers", id, burger_name, devoured, function(res) {
+			cb(res)
+		})
+	},
+
+	updateOne: function(id, burger_name, devoured, cb) {
+		orm.updateOne("burgers", id, burger_name, devoured, function(res) {
+			cb(res)
+		})
+	}
+}
+module.exports = burger;
