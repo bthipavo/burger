42a2261588d0e3e47ee8d81c1dbb1d94293847ea
bthipavo <bthipavo@gmail.com>
2018-03-27 23:15:17 -0700

    added burger and list html

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -2,5 +2,14 @@ var express = require('express')
 var router = express.Router()
 var burger = require('../models/burger.js')
 
+router.get("/", function(req, res) {
+	burger.selectAll(function(data) {
+		var hbsObject = {
+			burgers: data
+		}
+		console.log(hbsObject)
+		res.render("index", hbsObject)
+	})
+})
 
-module.exports = router
+module.exports = router 
